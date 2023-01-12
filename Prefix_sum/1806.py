4dc25a86baefa031582e09dfdcb43e4258209dcb
HiSeungmin <95595047+HiSeungmin@users.noreply.github.com>
2023-01-12 15:57:40 +0900

    1806

--- Prefix_sum/1806.py
+++ Prefix_sum/1806.py
@@ -1,28 +1,25 @@
 # 1806 : 부분합
 import sys
+
 N, S = map(int, sys.stdin.readline().split())
 arr = list(map(int, sys.stdin.readline().split()))
 
-len = N
+len = N+1
 sum = 0
 end = 0
 
 for start in range(N):
-    while sum <= S and end < N:
+    while sum < S and end < N:
         sum += arr[end]
-        end+=1
-    
-    if sum >= S and end-start<len:
-        print("end : ",end="")
-        print(end)
-        print("start : ",end="")
-        print(start)
-        len =end-start
+        end += 1
+
+    if sum >= S and end - start <= len:
+        len = end - start
 
     sum -= arr[start]
 
-if len==N:
+if len == N+1:
     print(0)
 else:
-    print(len)
+    print(len
 
