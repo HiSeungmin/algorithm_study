0d303692acb524373d5e1bf1a68e4140ef8def5d
HiSeungmin <95595047+HiSeungmin@users.noreply.github.com>
2023-01-12 21:01:58 +0900

    01-12

--- Prefix_sum/10986.py
+++ Prefix_sum/10986.py
@@ -1,4 +1,19 @@
 # 10986 : 나머지 합
 import sys
-N, M = map(int, sys.stdin.readline().split())
-arr = list(map(int, sys.stdin.readline().split()))
+input = sys.stdin.readline
+N, M = map(int, input().split())
+arr = list(map(int, input().split()))
+sum = 0
+num = [0] * M
+
+for i in range(N):
+  sum += arr[i]
+  num[sum % M] += 1
+
+result = num[0]
+
+# 나머지가 같은 구간 두 개 뽑기, iC2
+for i in num:
+  result += i*(i-1)//2
+
+print(result) 
---
+++ Prefix_sum/3020.py
@@ -0,0 +1,4 @@
+# 3020 : 개똥벌레
+import sys
+input = sys.stdin.readline
+N, H = map(int, input().split())
