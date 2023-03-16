8dee82a9f3dfa59da0df96008c2704fd4e3cd516
HiSeungmin <95595047+HiSeungmin@users.noreply.github.com>
2023-03-16 17:47:47 +0900

    0316

--- Graph/2468.py
+++ Graph/2468.py
@@ -1,6 +1,6 @@
 # 2468 : 안전 영역
 from collections import deque
-
+import copy
 dx = [0, 0, -1, 1]
 dy = [-1, 1, 0, 0]
 
@@ -16,9 +16,10 @@ for i in range(N):
 que = deque(sorted(element)) # 중복없이 원소들만 배열에 저장
 print(que)
 
-def bfs(n_arr, a, b):
+def bfs(n_arr, a, b, n):
+    #print("bfs함수 호출됨")
     q = deque()
-    arr[a][b] = 0
+    n_arr[a][b] = 0
     q.append([a, b])
 
     while q:
@@ -26,37 +27,41 @@ def bfs(n_arr, a, b):
         for k in range(4):
             nx = dx[k] + x
             ny = dx[k] + y
-
-            if 0<=nx<N and 0<=ny<N and n_arr[nx][ny] > num:
+            if 0<=nx<N and 0<=ny<N and n_arr[nx][ny] >= n:
                 n_arr[nx][ny] = 0
                 q.append([nx, ny])
 
-    print(arr)
-    print(n_arr)
-    print()
-    
-
-def init(num):
+def first_def(num):
+    n_arr = list()
     for i in range(N):
         for j in range(N):
             if 0 < arr[i][j] <= num:
                 arr[i][j] = 0
     
+    print(num)
+    print(arr)
     cnt = 0
 
     for i in range(N):
         for j in range(N):
             if arr[i][j] > 0:
-                n_arr = list(arr)
-                bfs(n_arr, i, j)
+                n_arr = copy.deepcopy(arr)
+                bfs(n_arr, i, j, num)
                 cnt += 1
+                # print("i : ",end="")
+                # print(i)
+                # print("j : ",end="")
+                # print(j)
+                # print("cnt : ",end="")
+                # print(cnt)
+                # print()
     return cnt
 
-result = []            
+result = []
 
 for num in que:
     #num = que.popleft()
-    result.append(init(num))
+    result.append(first_def(num))
 
 print(result)
 print(max(result))
