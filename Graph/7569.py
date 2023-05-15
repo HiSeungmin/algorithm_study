5028b9572209d9df9e69d666dbbcd5c712f1d970
HiSeungmin <95595047+HiSeungmin@users.noreply.github.com>
2023-05-15 16:31:02 +0900

    7569해결

--- Graph/7569.py
+++ Graph/7569.py
@@ -1,29 +1,45 @@
-# 7569 : 토마토(3차 배열)
+# 7569 : 토마토
 from collections import deque
 
-dx = [0, 0, -1, 1]
-dy = [-1, 1, 0, 0]
+dx = [0, 0, 1, -1, 0, 0]
+dy = [1, -1, 0, 0, 0, 0]
+dz = [0, 0, 0, 0, -1, 1]
 
 M, N, H = map(int, input().split(' '))
-field = []
+whole_field = []
 q = deque()
-
-for i in range(N*H):
-    field.append(list(map(int, input().split(' '))))
-    for j in range(M):
-        if field[i][j] == 1:
-            q.append([i,j])
+for k in range(H):
+    field = []
+    for i in range(N):
+        field.append(list(map(int, input().split())))
+        for j in range(M):
+            pass
+            if field[i][j] == 1:
+                q.append([k,i,j])
+    whole_field.append(field)
+#print(whole_field)
 
 def bfs():
-    
     while q:
-        x, y = q.popleft()
+        z, x, y = q.popleft()
+        for i in range(6): 
+            nx = x + dx[i]
+            ny = y + dy[i]
+            nz = z + dz[i]
 
-        for i in range(4):
-            nx = dx[i] + x
-            ny = dy[i] + y
+            if 0<=nx<N and 0<=ny<M and 0<=nz<H and whole_field[nz][nx][ny] == 0:
+                whole_field[nz][nx][ny] = whole_field[z][x][y] + 1
+                q.append([nz, nx, ny])
 
-            if 0<=nx<N*H and 0<=ny<M and field[nx][ny] == 0:
-                pass
+bfs()
+date = 0
 
+for g in whole_field:
+    for k in g:
+        for m in k:
+            if m == 0:
+                print(-1)
+                exit(0)
+        date = max(date, max(k))
 
+print(date-1)
