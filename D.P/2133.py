75b5755746d2bb8bef9fe9eea533bbc9a28aacae
HiSeungmin <95595047+HiSeungmin@users.noreply.github.com>
2023-05-18 18:53:39 +0900

    2133_dp(미완)

--- D.P/2133.py
+++ D.P/2133.py
@@ -1,1 +1,18 @@
 # 2133 : 타일 채우기
+n = int(input())
+d=[0]*(n+1)
+d[0] = 1
+d[1] = 0
+d[2] = 3
+
+def dp(x):
+    if d[x]!=0:
+        return d[x]
+    result = 3*dp(x-2)
+    for i in range(3, x+1):
+        if i%2==0:
+            result += (2*dp(x-i))
+    d[x] = result
+    return result
+
+print(dp(n))
