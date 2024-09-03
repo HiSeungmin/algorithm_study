c73724107baa5982eea6e7a982a1fea9badad7cd
HiSeungmin <osminosmin7180@gmail.com>
2024-09-03 22:01:10 +0900

    delete test

--- Test/practice.py
+++
@@ -1,9 +0,0 @@
-def solution(numbers):
-    answer = 0
-    arr = list(numbers)
-    n = len(arr)
-    for i in range(
-    return answer
-
-
-print(solution("17"))
--- Test/string.py
+++
@@ -1,5 +0,0 @@
-k = int(input())
-for _ in range(k):
-    n = input()
-    print(n[0],end="")
-    print(n[-1])
